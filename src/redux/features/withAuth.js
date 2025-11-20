3717ed46a9d50f8f30dc840a4ba257b47b3a9cef
MohammedRasif <mohammed660012@gmail.com>
2025-11-20 15:13:47 +0600

    add somenting new

--- src/redux/features/withAuth.js
+++ src/redux/features/withAuth.js
@@ -3,8 +3,8 @@ export const sqQuery = createApi({
   reducerPath: "sqQuery",
   baseQuery: fetchBaseQuery({
     // baseUrl: "https://well-anteater-happy.ngrok-free.app/",
-    // baseUrl: "http://31.97.39.215/",
-    baseUrl: "http://10.10.13.80:8002",
+    baseUrl: "http://31.97.39.215/",
+    // baseUrl: "http://10.10.13.80:8002",
 
     prepareHeaders: (headers, { endpoint }) => {
       headers.set("ngrok-skip-browser-warning", "true");
