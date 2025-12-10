025006fc9e57733f3ba3ede17939fcf091311253
MohammedRasif <mohammed660012@gmail.com>
2025-12-10 15:14:39 +0600

    ad

--- src/components/created-plan-card.jsx
+++ src/components/created-plan-card.jsx
@@ -149,9 +149,9 @@ export default function CreatedPlanCard({ plan, setCreatedPlans }) {
               {t("approval_status")}:{" "}
               <span
                 className={`inline-flex items-center px-2 py-0.5 rounded-full text-[14px] font-medium ${
-                  plan.approval_status === "Rejected"
+                  plan.approval_status === "Rifiutato"
                     ? "bg-red-100 text-red-700"
-                    : plan.approval_status === "Pending"
+                    : plan.approval_status === "In attesa"
                     ? "bg-gray-100 text-black"
                     : "bg-green-100 text-green-700"
                 }`}
